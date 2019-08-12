c83185f0d4141532850ea565226c18bcc8608e2e
Wei Ye <weiye@cs.ucsb.edu>
2019-08-12 16:05:43 -0700

    Update breadth_first_search.py

--- code/breadth_first_search.py
+++ code/breadth_first_search.py
@@ -1,30 +1,34 @@
 import networkx as nx
 import copy
 from collections import deque
+from operator import itemgetter
 
-__all__ = ['bfs_edges', 'bfs_tree', 'bfs_predecessors', 'bfs_successors']
+__all__ = ['bfs_edges', 'bfs_tree']
 
 
-def generic_bfs_edges(G, label, source, neighbors=None, depth_limit=None):
+def generic_bfs_edges(G, label, eigen_cen, source, neighbors=None, depth_limit=None):
     
     visited = {source}
     if depth_limit is None:
         depth_limit = len(G)
 
     neigh = list(neighbors(source))
-    #print(neigh)
+
     neighlabel = []
     for nei in neigh:
-        neighlabel.append(label[int(nei)])
-    #print(neighlabel)
-    neighindex = sorted(range(len(neighlabel)), key=lambda k: neighlabel[k])
+        neighbor_dict = {}
+        neighbor_dict['vertex'] = int(nei)
+        neighbor_dict['label'] = label[int(nei)]
+        neighbor_dict['eigencentrality'] = eigen_cen[int(nei)]
+        neighbor_list.append(neighbor_dict)
+
+    neighbor_list_sorted = sorted(neighbor_list, key=itemgetter('label', 'eigencentrality'))
     sortedneighbor = []
-    for ele in neighindex:
-        sortedneighbor.append(neigh[ele])
+    for neighbor_dict_sorted in neighbor_list_sorted:
+        vertex = neighbor_dict_sorted['vertex']
+        sortedneighbor.append(vertex)
 
     queue = deque([(source, depth_limit, iter(sortedneighbor))])
-    #print(type(neighbors(source)))
-    #print(list(iter(sortedneighbor)))
 
     while queue:
         parent, depth_now, children = queue[0]
@@ -35,26 +39,32 @@ def generic_bfs_edges(G, label, source, neighbors=None, depth_limit=None):
                 visited.add(child)
                 if depth_now > 1:
                     chil = list(neighbors(child))
-                    chillabel = []
-                    for ch in chil:
-                        chillabel.append(label[int(ch)])
+                    child_list = []
 
-                    chilindex = sorted(range(len(chillabel)), key=lambda k: chillabel[k])
-                    sortedneighbor = []
-                    for ele in chilindex:
-                        sortedneighbor.append(chil[ele])
-                    
-                    queue.append((child, depth_now - 1, iter(sortedneighbor)))
+                    for chi in chil:
+                        child_dict = {}
+                        child_dict['vertex'] = int(chi)
+                        child_dict['label'] = label[int(chi)]
+                        child_dict['eigencentrality'] = eigen_cen[int(chi)]
+                        child_list.append(child_dict)
+
+                    child_list_sorted = sorted(child_list, key=itemgetter('label', 'eigencentrality'))
+                    sortedchild = []
+                    for child_dict_sorted in child_list_sorted:
+                        vertex = child_dict_sorted['vertex']
+                        sortedchild.append(vertex)
+
+                    queue.append((child, depth_now - 1, iter(sortedchild)))
         except StopIteration:
             queue.popleft()
 
 
-def bfs_edges(G, label, source, reverse=False, depth_limit=None):
+def bfs_edges(G, label, eigen_cen, source, reverse=False, depth_limit=None):
     
     if reverse and G.is_directed():
         successors = G.predecessors
     else:
         successors = G.neighbors
     # TODO In Python 3.3+, this should be `yield from ...`
-    for e in generic_bfs_edges(G, label, source, successors, depth_limit):
+    for e in generic_bfs_edges(G, label, eigen_cen, source, successors, depth_limit):
         yield e
